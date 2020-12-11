669f32062b70725ec6f8c0b825d10cbb92e7da96
Marcilio <marciliorsilva93@gmail.com>
2020-12-11 14:50:10 -0300

    criando evento de click e as funções de vira e desvira das cartas

--- script.js
+++ script.js
@@ -3,8 +3,22 @@ let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
 
+//função que reseta o tabuleiro
+let resetBoard = () => {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+//função que embaralha as cartas
+(function shuffle() {
+    cards.forEach((card) => {
+        let ramdomPosition = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPosition;
+    })
+})();
+
 //função para virar carta
-let virarCarta = () => {
+function flipCard() {
     if(lockBoard) return;
     if(this === firstCard) return;
 
@@ -17,5 +31,22 @@ let virarCarta = () => {
 
     secondCard = this;
     hasFlippedCard = false;
-    checkForMatch();
+   
+}
+
+//adiciona evento de clique na carta
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard)
+});
+
+//funcão que desvira as cartas
+unflipCards = () => {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
 }
